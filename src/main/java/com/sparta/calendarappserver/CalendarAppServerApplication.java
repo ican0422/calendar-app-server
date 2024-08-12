bd956fe5cf166deadcb411f597ede6e8731e0b93
ican0422 <ican0422@gmail.com>
2024-08-12 20:21:13 +0900

    [add] : git init

---
+++ src/main/java/com/sparta/calendarappserver/CalendarAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.calendarappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/calendarappserver/CalendarAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.calendarappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
